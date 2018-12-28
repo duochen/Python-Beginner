032a3bc855e6c2d4327160d442a3ddae23e9bf35
duochen <duochen@yahoo.com>
2018-12-28 11:05:09 -0600

    check in code

---
+++ Lecture01/Homework/solution03.py
@@ -0,0 +1,8 @@
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+# Sample Output
+#
+# Input the radius of the circle : 1.1                                                                          
+# The area of the circle with radius 1.1 is: 3.8013271108436504 
