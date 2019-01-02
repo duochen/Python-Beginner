5ef1da9190b3953c93c4653b2e9c0f236bb85180
duochen <duochen@yahoo.com>
2019-01-02 16:36:45 -0600

    check in code

---
+++ Lecture02/Homework/solution05.py
@@ -0,0 +1,10 @@
+tuplex = ("w", 3, "r", "e", "s", "o", "u", "r", "c", "e")
+print(tuplex)
+
+#Get item (4th element)of the tuple by index
+item = tuplex[3]
+print(item)     # => e
+
+#Get item (4th element from last)by index negative
+item1 = tuplex[-4]
+print(item1)    # => u
---
+++ Lecture02/Homework/solution06.py
@@ -0,0 +1,3 @@
+n = "246.2458"
+print(float(n))      # => 246.2458
+print(int(float(n))) # => 246
