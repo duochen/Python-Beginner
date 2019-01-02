452201320a1c1c0ecb07b95deff33264ba07a3ad
duochen <duochen@yahoo.com>
2019-01-02 16:49:12 -0600

    check in code

---
+++ Lecture02/Homework/solution08.py
@@ -0,0 +1,3 @@
+str = "https://www.w3resource.com/python-exercises/string"
+print(str.rsplit('/', 1)[0])   # => https://www.w3resource.com/python-exercises
+print(str.rsplit('-', 1)[0])   # => https://www.w3resource.com/python
