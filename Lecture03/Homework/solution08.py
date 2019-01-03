5f4c68a1b4259f81a8618ad6bd7efd1a29c32d7a
duochen <duochen@yahoo.com>
2019-01-02 20:29:11 -0600

    check in code

---
+++ Lecture03/Homework/solution08.py
@@ -0,0 +1,7 @@
+def test_range(n):
+    if n in range(3,9):
+        print( " %s is in the range"%str(n))
+    else :
+        print("The number is outside the given range.")
+
+test_range(5)  # =>  5 is in the range
