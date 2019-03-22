0f942c8a1168318a5c3a082ad0eb2415fc01e8b0
Duo Chen <DChen@slb.com>
2019-03-22 14:56:12 -0500

    check in code

--- Lecture08/Exercises/with.py
+++ Lecture08/Exercises/with.py
@@ -1,11 +1,24 @@
+import os
+
 def read_file_using_exception(filename):
     try:
         infile = open(filename)
         data = infile.read()
+        print(data)
     finally:
         infile.close()
 
 def read_file_using_with(filename):
     with open(filename) as infile:
         data = infile.read()
+        print(data)
 
+def get_file_full_path(filename):
+    currentFilePath = os.path.abspath(__file__)
+    currentFileDir = os.path.dirname(currentFilePath)
+    return os.path.join(currentFileDir, filename)
+
+file_with_full_path = get_file_full_path("with.py")
+
+read_file_using_with(file_with_full_path)
+read_file_using_exception(file_with_full_path)
