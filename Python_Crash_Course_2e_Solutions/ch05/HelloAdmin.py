86c0386c036b9633c5de5f90615ca7336b22bb44
Duo Chen <dchen@slb.com>
2019-08-13 10:20:20 -0500

    check in code

---
+++ Python_Crash_Course_2e_Solutions/ch05/HelloAdmin.py
@@ -0,0 +1,7 @@
+usernames = ['eric', 'willie', 'admin', 'erin', 'ever']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print("Hello " + username + ", thank you for logging in again!")
