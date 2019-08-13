b70d5bf1fd27c75992674a432d95849da8b52aca
Duo Chen <dchen@slb.com>
2019-08-13 09:55:12 -0500

    check in code

---
+++ Python_Crash_Course_2e_Solutions/ch03/Greetings.py
@@ -0,0 +1,10 @@
+names = ['ron', 'tyler', 'dani']
+
+msg = "Hello, " + names[0].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[1].title() + "!"
+print(msg)
+
+msg = "Hello, " + names[2].title() + "!"
+print(msg)
---
+++ Python_Crash_Course_2e_Solutions/ch03/GuestList.py
@@ -0,0 +1,10 @@
+guests = ['guido van rossum', 'jack turner', 'lynn hill']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
