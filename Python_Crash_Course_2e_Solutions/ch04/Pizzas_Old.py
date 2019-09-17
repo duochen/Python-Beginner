3936827dd1204725b2bec5cfa63ccc4e4c07b30c
Duo Chen <duochen@yahoo.com>
2019-09-16 21:35:13 -0500

    check in code

---
+++ Python_Crash_Course_2e_Solutions/ch04/Pizzas_Old.py
@@ -0,0 +1,15 @@
+favorite_pizzas = ['pepperoni', 'hawaiian', 'veggie']
+
+# Print the names of all the pizzas.
+print(favorite_pizzas[0])
+print(favorite_pizzas[1])
+print(favorite_pizzas[2])
+
+print("\n")
+
+# Print a sentence about each pizza.
+print("I really love " + favorite_pizzas[0] + " pizza!")
+print("I really love " + favorite_pizzas[1] + " pizza!")
+print("I really love " + favorite_pizzas[2] + " pizza!")
+
+print("\nI really love pizza!")
