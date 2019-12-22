a82fd257dd205395f0b3aba9e4b22ee56871385f
Duo Chen <duochen@yahoo.com>
2019-12-22 11:46:29 -0600

    check in code

--- CS41/Python5.1-OOP/MyClass.py
+++ CS41/Python5.1-OOP/MyClass.py
@@ -8,3 +8,10 @@ class MyClass:
 print(MyClass.num)     # => 123245       (int object)
 print(MyClass.greet)   # => <function f> (function object)
 
+x = MyClass()
+x.greet()     # 'Hello world!'
+print(type(x.greet))       # method
+print(type(MyClass.greet))  # function
+print(x.num is MyClass.num)   # True
+print(x.greet is MyClass.greet)  # False
+
