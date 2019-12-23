811cc344c7d38aa80da31d0591f62d0c78b4e285
Duo Chen <duochen@yahoo.com>
2019-12-22 18:29:00 -0600

    check in code

--- CS41/Python4.1-FP/Debug.py
+++ CS41/Python4.1-FP/Debug.py
@@ -1,4 +1,3 @@
-
 def debug(function):
     def wrapper(*args, **kwargs):
         print("Arguments:", args, kwargs)
@@ -8,7 +7,17 @@ def debug(function):
 
 @debug
 def foo(a, b, c=1):
-    return (a + b) *c
+    return (a + b) * c
 
 r = foo(5, 3, c=2)  # prints "Arguments: (5, 3) {'c': 2}"
 print(r)
+
+def goo(a, b, c=1):
+    return (a + b) * c
+
+goo = debug(goo)
+x = goo(2, 3)
+print(x)
+y = goo(2, 1, c=3)
+print(y)
+print(goo)
