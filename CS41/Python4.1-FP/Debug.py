4018b2991d028ac375dc7778aafd65afe8ad12c3
Duo Chen <duochen@yahoo.com>
2019-12-22 18:25:38 -0600

    check in code

---
+++ CS41/Python4.1-FP/Debug.py
@@ -0,0 +1,14 @@
+
+def debug(function):
+    def wrapper(*args, **kwargs):
+        print("Arguments:", args, kwargs)
+        return function(*args, **kwargs)
+
+    return wrapper
+
+@debug
+def foo(a, b, c=1):
+    return (a + b) *c
+
+r = foo(5, 3, c=2)  # prints "Arguments: (5, 3) {'c': 2}"
+print(r)
