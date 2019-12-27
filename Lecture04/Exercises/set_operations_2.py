0d7fdaaf30ac33b971e2cbe34aae1c00a1225146
Duo Chen <duochen@yahoo.com>
2019-12-27 12:56:29 -0600

    check in code

--- Lecture04/Exercises/set_operations_2.py
+++ Lecture04/Exercises/set_operations_2.py
@@ -2,4 +2,4 @@ studentList = {'danish', 'jason', 'sarah', 'mike', 'arthur', 'jack'}
 placedStudList = {'mike', 'danish', 'arthur'}
 
 notPlacedStudList = set(studentList) - set(placedStudList)
-print("Students yet to get job", notPlacedStudList)
+print("Students yet to get job", notPlacedStudList) # => {'jack', 'jason', 'sarah'}
