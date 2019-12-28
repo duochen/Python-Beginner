31dfa35957073f0db4d5788484c08b888d95eef9
Duo Chen <duochen@yahoo.com>
2019-12-28 10:33:48 -0600

    check in code

---
+++ Challenging_Exercises/Level1/Question006.py
@@ -0,0 +1,10 @@
+class Person:
+    # Define the class parameter "name"
+    name = "Person"
+    
+    def __init__(self, name = None):
+        # self.name is the instance parameter
+        self.name = name
+
+jeffrey = Person("Jeffrey")
+print(f"{Person.name} name is {jeffrey.name}")
