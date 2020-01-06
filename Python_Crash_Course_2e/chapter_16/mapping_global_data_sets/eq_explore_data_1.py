da97fbd1d8b8edc5105ea9aaec79814ac68b26b1
Duo Chen <dchen@slb.com>
2020-01-06 15:34:19 -0600

    check in code

---
+++ Python_Crash_Course_2e/chapter_16/mapping_global_data_sets/eq_explore_data_1.py
@@ -0,0 +1,10 @@
+import json
+
+# Explore the structure of the data.
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+readable_file = 'data/readable_eq_data.json'    
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
---
+++ Python_Crash_Course_2e/chapter_16/mapping_global_data_sets/eq_explore_data_3.py
@@ -0,0 +1,14 @@
+import json
+
+# Explore the structure of the data.
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])    
