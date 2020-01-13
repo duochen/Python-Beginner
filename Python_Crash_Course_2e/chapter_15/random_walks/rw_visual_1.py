ddcf54ab9a99b23cfeddbb3e5d0417548cf83ad0
Duo Chen <duochen@yahoo.com>
2020-01-12 22:17:35 -0600

    check in code

---
+++ Python_Crash_Course_2e/chapter_15/random_walks/rw_visual_1.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+
+    plt.show()
