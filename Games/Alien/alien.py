60a31fe80299dcf60d395b9fff77035bbffca574
Duo <duochen@yahoo.com>
2020-05-14 00:36:38 -0500

    check in code

--- Games/Alien/alien.py
+++ Games/Alien/alien.py
@@ -60,7 +60,7 @@ alienImage = pygame.image.load('alien.png')
 
 # Show the "Start" screen.
 windowSurface.fill(BACKGROUNDCOLOR)
-drawText('Dodger', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
+drawText('Alien Invasion', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3))
 drawText('Press a key to start.', font, windowSurface, (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3) + 50)
 pygame.display.update()
 waitForPlayerToPressKey()
